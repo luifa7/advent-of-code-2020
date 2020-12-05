c23a40c84c999d5e4bc814d114053ac18be7a037
Luis Alfredo Godoy Alvarez <luisalfredo.godoyalvarez@paessler.com>
2020-12-05 18:45:34 +0100

    move pass methods to password type

--- adventSecondDay.go
+++ adventSecondDay.go
@@ -8,49 +8,25 @@ import (
 
 var pathToInputSecondDay = "/Users/lgodoyalvarez/other_projects/gocode/advent/input_02"
 
-func parsePassLinesToStruct() []Password {
+func parsePassLinesToStruct() []password {
 	resutl := readFileLines(pathToInputSecondDay)
-	var toReturn []Password
+	var toReturn []password
 	for _, r := range resutl {
 		s := strings.Fields(r)
 		rangeForLetter := strings.Split(s[0], "-")
 		min, _ := strconv.Atoi(rangeForLetter[0])
 		max, _ := strconv.Atoi(rangeForLetter[1])
-		newPass := Password{min, max, string(s[1][0]), s[2]}
+		newPass := password{min, max, string(s[1][0]), s[2]}
 		toReturn = append(toReturn, newPass)
 	}
 	return toReturn
-}
-
-func isPassOkOldRule(pass Password) bool {
-	timesLetterInPass := strings.Count(pass.pass, pass.letter)
-	if pass.minTimes <= timesLetterInPass && pass.maxTimes >= timesLetterInPass {
-		return true
-	}
-	return false
-}
-
-func isPassOkNewRule(pass Password) bool {
-	letterOnMinPossition := string(pass.pass[pass.minTimes-1])
-	letterOnMaxPossition := string(pass.pass[pass.maxTimes-1])
-
-	if letterOnMinPossition == pass.letter {
-		if letterOnMaxPossition == pass.letter {
-			return false
-		}
-		return true
-	} else if letterOnMaxPossition == pass.letter {
-		return true
-	}
-
-	return false
 }
 
 func countCorrectPassOldRule() int {
 	passs := parsePassLinesToStruct()
 	counter := 0
 	for _, pass := range passs {
-		if isPassOkOldRule(pass) {
+		if pass.isPassOkOldRule() {
 			counter++
 		}
 	}
@@ -62,7 +38,7 @@ func countCorrectPassNewRule() int {
 	passs := parsePassLinesToStruct()
 	counter := 0
 	for _, pass := range passs {
-		if isPassOkNewRule(pass) {
+		if pass.isPassOkNewRule() {
 			counter++
 		}
 	}
--- password.go
+++ password.go
@@ -1,10 +1,36 @@
 package main
 
-// Password is a new type
+import "strings"
+
+// password is a new type
 // which is a structure to collect validations
-type Password struct {
+type password struct {
 	minTimes int
 	maxTimes int
 	letter   string
 	pass     string
+}
+
+func (p password) isPassOkNewRule() bool {
+	letterOnMinPossition := string(p.pass[p.minTimes-1])
+	letterOnMaxPossition := string(p.pass[p.maxTimes-1])
+
+	if letterOnMinPossition == p.letter {
+		if letterOnMaxPossition == p.letter {
+			return false
+		}
+		return true
+	} else if letterOnMaxPossition == p.letter {
+		return true
+	}
+
+	return false
+}
+
+func (p password) isPassOkOldRule() bool {
+	timesLetterInPass := strings.Count(p.pass, p.letter)
+	if p.minTimes <= timesLetterInPass && p.maxTimes >= timesLetterInPass {
+		return true
+	}
+	return false
 }
